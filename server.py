f8366a56e2a52dffac51cf2ae9148f44d8f767af
Danya Generalov <danya02@me.com>
2016-11-04 13:21:42 +0300

    Miscellaneous fixes regarding data transmission and movement.

--- server.py
+++ server.py
@@ -7,16 +7,58 @@ import os
 import time
 SERVER = "127.0.0.1"
 TOPIC = "drone1"
-base.init()
 global motorstat
 motorstat = "STOP"
 global base
-base = gpiozero.Robot(left=(5, 6), right=(7, 8))
+
+
+class ArduinoMotor:
+	def __init__(self):
+		self.dirleft = gpiozero.LED(17)
+		self.dirright = gpiozero.LED(22)
+		self.motorleft = gpiozero.LED(18)
+		self.motorright = gpiozero.LED(23)
+
+	def __exit__(self):
+		self.dirright.close()
+		self.dirleft.close()
+		self.motorright.close()
+		self.motorleft.close()
+
+	def forward(self):
+		self.dirright.off()
+		self.dirleft.off()
+		self.motorright.on()
+		self.motorleft.on()
+
+	def back(self):
+		self.dirright.on()
+		self.dirleft.on()
+		self.motorright.on()
+		self.motorleft.on()
+
+	def left(self):
+		self.dirright.off()
+		self.dirleft.on()
+		self.motorright.on()
+		self.motorleft.on()
+
+	def right(self):
+		self.dirright.on()
+		self.dirleft.off()
+		self.motorright.on()
+		self.motorleft.on()
+
+	def stop(self):
+		self.motorright.off()
+		self.motorleft.off()
+
+base = ArduinoMotor()
 global display_relay
 display_relay = gpiozero.LED(3)
 kill_switch = gpiozero.LED(4, initial_value=True)
 c = mqtt.Client()
-c.will_set(TOPIC, payload="DEAD", qos=2)
+c.will_set(TOPIC, "DEAD", qos=2)
 c.connect(SERVER)
 c.subscribe(TOPIC, qos=1)
 c.publish("ALIVE", TOPIC)
@@ -29,67 +71,67 @@ def on_message(client, userdata, m):
     global base
     m = m.payload
     try:
-        if "ACK" in m:
-            client.publish(m.split(":")[0]+":"+"RACK", TOPIC)
+        if b"ACK" in m:
+            client.publish(TOPIC, str(m.split(b":")[0][2:-1])+":"+"RACK")
             raise IndexError
-        elif "MOVE" in m:
-            if "FORWARD" in m:
+        elif b"MOVE" in m:
+            if b"FORWARD" in m:
                 base.forward()
                 motorstat = "FORWARD"
-            elif "BACK" in m:
+            elif b"BACK" in m:
                 base.back()
                 motorstat = "BACK"
-            elif "LEFT" in m:
+            elif b"LEFT" in m:
                 base.left()
                 motorstat = "LEFT"
-            elif "RIGHT" in m:
+            elif b"RIGHT" in m:
                 base.right()
                 motorstat = "RIGHT"
-        elif "STOP" in m:
+        elif b"STOP" in m:
             base.stop()
             motorstat = "STOP"
-        elif "MOTORSTAT" in m:
-            client.publish(motorstat, TOPIC)
+        elif b"MOTORSTAT" in m:
+            client.publish(TOPIC, str(m.split(b":")[0])[2:-1]+":"+motorstat)
             raise IndexError
-        elif "DISPLAY" in m:
-            if "ON" in m:
+        elif b"DISPLAY" in m:
+            if b"ON" in m:
                 display_relay.on()
-            elif "OFF" in m:
+            elif b"OFF" in m:
                 display_relay.on()
-            elif "STATUS" in m:
-                client.publish("ON" if display_relay.is_lit else "OFF", TOPIC)
+            elif b"STATUS" in m:
+                client.publish(TOPIC, str(m.split(b":")[0])[2:-1]+("ON" if display_relay.is_lit else "OFF"))
                 raise IndexError
-        elif "REBOOT" in m:
+        elif b"REBOOT" in m:
             os.popen("shutdown -r now")
             time.sleep(10)
-            raise StandardError
-        elif "POWERCYCLE" in m:
+            raise IOError
+        elif b"POWERCYCLE" in m:
             kill_switch.off()
             time.sleep(5)
-            raise StandardError
-        elif "VIDEOIN" in m:
-            if "ON" in m:
+            raise IOError
+        elif b"VIDEOIN" in m:
+            if b"ON" in m:
                 # videoin.start()
                 videoinstat = True
-            elif "OFF" in m:
+            elif b"OFF" in m:
                 # videoin.stop()
                 videoinstat = False
-            elif "STATUS" in m:
-                client.publish("ON" if videoinstat else "OFF", TOPIC)
+            elif b"STATUS" in m:
+                client.publish(TOPIC, str(m.split(b":")[0])[2:-1]+("ON" if videoinstat else "OFF"))
                 raise IndexError
-        elif "VIDEOOUT" in m:
-            if "ON" in m:
+        elif b"VIDEOOUT" in m:
+            if b"ON" in m:
                 # videoout.start()
                 pass
-            elif "OFF" in m:
+            elif b"OFF" in m:
                 # videoout.stop()
                 pass
-    except StandardError:
-        client.publish(m.split(":")[0]+":"+"FAIL", TOPIC)
+    except IOError:
+        client.publish(TOPIC, str(m.split(b":")[0])[2:-1]+":"+"FAIL")
     except IndexError:
         pass
     else:
-        client.publish(m.split(":")[0]+":"+"ACK", TOPIC)
+        client.publish(TOPIC, str(m.split(b":")[0])[2:-1]+":"+"ACK")
 
 c.on_message = on_message
 c.loop_forever()
